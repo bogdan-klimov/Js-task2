8db9755879835aff5897bb0b7e1442bc7a5e6f7b
bogdan-klimov <bodyaklimov333@gmail.com>
2020-07-28 20:31:11 +0300

    first commit

---
+++ idex.js
@@ -0,0 +1,65 @@
+//////////////////////////////1
+console.log ("First task:");
+
+for (let i=2; i < 18; i++) {
+	if ( i%2 == 0 ) {
+		console.log(i);
+	}
+}
+//////////////////////////////2
+console.log ("Second task:");
+
+let c = 0;
+
+while ( c < 5 ) {
+    console.log(c);
+    c++
+}
+//////////////////////////////3
+console.log ("Third task:"); 
+
+let m = 10;
+
+let n;
+
+if (m > 50) {
+    n = "Большое";
+    console.log (n);
+} else {
+    n = "Маленькое";
+    console.log (n);
+}
+//////////////////////////////4
+console.log ("The fourth task:")
+
+let k = 45;
+
+while ( k < 68) {
+    console.log(k);
+    k++
+}
+//////////////////////////////5
+console.log ("The fifth task:")
+
+let b = 45;
+
+while (b < 671) {
+    if  ( b%10 == 0) {
+       console.log(b); 
+    } 
+    b++
+}
+//////////////////////////////6
+console.log ("The sixth task, first part:")
+
+for (let k = 45; k < 68; k++) {
+    console.log(k);
+}
+
+console.log ("The sixth task, second part:")
+
+for (let b = 45; b < 671; b++) {
+    if (b%10 == 0) {
+        console.log(b);
+    }
+}
